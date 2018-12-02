031674be8c77bfca946118040228a500078f67c5
David Betteridge <DavidaBetteridge@gmail.com>
2018-12-02 21:32:55 +0000

    Day1 and Day2

---
+++ Day1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = System.IO.File.ReadAllLines("Frequencies.txt").Select(a => int.Parse(a));
+            var total = lines.Sum();
+            Console.WriteLine(total);
+
+            var totalsSeen = new HashSet<int>();
+            var runningTotal = 0;
+
+            foreach (var change in Cycle(lines))
+            {
+                runningTotal += change;
+                if (totalsSeen.Contains(runningTotal))
+                {
+                    Console.WriteLine(runningTotal);
+                }
+                else
+                {
+                    totalsSeen.Add(runningTotal);
+                }
+            }
+
+            //81972
+
+
+        }
+
+
+        public static IEnumerable<int> Cycle(IEnumerable<int> numbers)
+        {
+            while (true)
+            {
+                foreach (var item in numbers)
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = System.IO.File.ReadAllLines("Boxes.txt");
+
+            var groupedLines = lines.Select(line => line.GroupBy(c => c));
+            var numberOfTwo = groupedLines.Where(line => line.Any(c => c.Count() == 2)).Count();
+            var numberOfThree = groupedLines.Where(line => line.Any(c => c.Count() == 3)).Count();
+            var answer = numberOfTwo * numberOfThree;
+            Console.WriteLine(answer);
+
+            var pairs = from l1 in lines
+                        from l2 in lines
+                        where Distance(l1, l2) == 1
+                        select RemoveDifferences(l1, l2);
+
+            Console.WriteLine(pairs.First());
+        }
+
+        private static int Distance(string line1, string line2) =>
+            line1
+                .Zip(line2, (a, b) => (a, b))
+                .Count(ab => ab.a != ab.b);
+        private static string RemoveDifferences(string line1, string line2) =>
+            string.Join("", line1
+                            .Zip(line2, (a, b) => (a, b))
+                            .Where(ab => ab.a == ab.b)
+                            .Select(ab => ab.a));
+    }
+}
