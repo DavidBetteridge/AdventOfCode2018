d4f49bc5020673a4fa210bdc482a62709f49683f
David Betteridge <DavidaBetteridge@gmail.com>
2018-12-07 15:49:10 +0000

    Refactor

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -7,51 +7,53 @@ namespace Day7
     {
         static void Main(string[] args)
         {
-            var steps = System.IO.File.ReadAllLines("Steps.txt")
-                                            .Select((line, index) => new Step(line));
+            System.Console.WriteLine($"Part 1 -> {Part1()}");
+            System.Console.WriteLine($"Part 2 -> {Part2()}");
+            System.Console.ReadKey();
+        }
 
+        static string Part1()
+        {
+            var dependencies = WorkoutDependencies("steps.txt");
+
+            var answer = "";
+            while (dependencies.Any())
+            {
+                var possible = dependencies.Where(d => !d.Value.Any()).OrderBy(d => d.Key).First().Key;
+
+                foreach (var item in dependencies.Values)
+                {
+                    if (item.Contains(possible))
+                        item.Remove(possible);
+                }
+
+                dependencies.Remove(possible);
+
+                answer += possible;
+            }
+
+            return answer;
+        }
+
+
+        static int Part2()
+        {
             const int StepOverHead = 60;
             const int NumberOfWorkers = 5;
             var workers = new Worker[NumberOfWorkers] { new Worker(), new Worker(), new Worker(), new Worker(), new Worker() };
 
-            //const int StepOverHead = 0;
-            //const int NumberOfWorkers = 2;
-            //var workers = new Worker[NumberOfWorkers] { new Worker(), new Worker() };
+            var dependencies = WorkoutDependencies("steps.txt");
 
-            var allSteps = new HashSet<string>();
-            var depends = new Dictionary<string, List<string>>();
-            foreach (var step in steps)
-            {
-                allSteps.Add(step.FromStep);
-                if (!depends.TryGetValue(step.ToStep, out var dependsOn))
-                {
-                    dependsOn = new List<string>();
-                    depends.Add(step.ToStep, dependsOn);
-                }
-                dependsOn.Add(step.FromStep);
-            }
-
-            var firsts = allSteps.Where(s => !depends.ContainsKey(s));
-            foreach (var first in firsts)
-            {
-                var dependsOn = new List<string>();
-                depends.Add(first, dependsOn);
-            }
-
-            System.Console.WriteLine($"Second  Worker 1     Worker 2    Done");
             var done = "";
             var timeTaken = 0;
-            while (depends.Any() || workers.Any(w => w.TimeRemaining > 0))
+            while (dependencies.Any() || workers.Any(w => w.TimeRemaining > 0))
             {
-
-                // Another second has passed.  Decrease the TimeRemaining on all workers.
-                // If they have now reached zero,  then they are free,  and the task is complete.
                 foreach (var worker in workers.Where(w => w.TimeRemaining > 0))
                 {
                     worker.TimeRemaining--;
                     if (worker.TimeRemaining == 0)
                     {
-                        foreach (var item in depends.Values)
+                        foreach (var item in dependencies.Values)
                         {
                             if (item.Contains(worker.CurrentActivity))
                             {
@@ -65,31 +67,30 @@ namespace Day7
                     }
                 }
 
-                // Is there anywork for our workers todo?
                 foreach (var worker in workers.Where(w => string.IsNullOrWhiteSpace(w.CurrentActivity)))
                 {
-                    var nextJob = depends.Where(d => !d.Value.Any()).OrderBy(d => d.Key).FirstOrDefault();
-                    if (!string.IsNullOrWhiteSpace(nextJob.Key))
+                    var nextJob = dependencies.Where(d => !d.Value.Any()).OrderBy(d => d.Key).FirstOrDefault();
+                    if (string.IsNullOrWhiteSpace(nextJob.Key))
+                    {
+                        break;  //no point looking at the other workers if there is no more work to do.
+                    }
+                    else
                     {
                         worker.CurrentActivity = nextJob.Key;
                         worker.TimeRemaining = StepOverHead + (nextJob.Key[0] - 'A') + 1;
-                        depends.Remove(nextJob.Key);
+                        dependencies.Remove(nextJob.Key);
                     }
                 }
-
-                //System.Console.WriteLine($"{timeTaken}  {workers[0].CurrentActivity}    {workers[1].CurrentActivity}    {done}");
-                System.Console.WriteLine($"{timeTaken}  {workers[0].CurrentActivity}    {workers[1].CurrentActivity}    {workers[2].CurrentActivity}    {workers[3].CurrentActivity}    {workers[4].CurrentActivity}    {done}");
-
                 timeTaken++;
-
             }
-            System.Console.Write(timeTaken - 1);
 
+            return timeTaken - 1;
         }
-        void Part1()
+
+        private static Dictionary<string, List<string>> WorkoutDependencies(string filename)
         {
-            var steps = System.IO.File.ReadAllLines("Steps.txt")
-                                            .Select((line, index) => new Step(line));
+            var steps = System.IO.File.ReadAllLines(filename)
+                                .Select((line, index) => new Step(line));
 
             var allSteps = new HashSet<string>();
             var depends = new Dictionary<string, List<string>>();
@@ -111,23 +112,9 @@ namespace Day7
                 depends.Add(first, dependsOn);
             }
 
-            while (depends.Any())
-            {
-                var possible = depends.Where(d => !d.Value.Any()).OrderBy(d => d.Key).First().Key;
-
-                foreach (var item in depends.Values)
-                {
-                    if (item.Contains(possible))
-                    {
-                        item.Remove(possible);
-                    }
-                }
-
-                depends.Remove(possible);
-                System.Console.Write(possible);
-            }
-
+            return depends;
         }
+
     }
 
     internal class Worker
