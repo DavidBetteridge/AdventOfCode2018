52069d7eafd6f7286d8b9e46d19ce35b4a059d38
David Betteridge <DavidaBetteridge@gmail.com>
2018-12-09 10:52:28 +0000

    Part 2

---
+++ Day9/Circle.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day9
+{
+    class Circle
+    {
+        private readonly LinkedList<int> _marbles;
+        private LinkedListNode<int> _current;
+        public Circle(int maxSize)
+        {
+            _marbles = new LinkedList<int>();
+            _current = new LinkedListNode<int>(0);
+            _marbles.AddFirst(_current);
+        }
+
+        public void AddMarble(int marble)
+        {
+            var insertAfter = _current.Next;
+            if (insertAfter == null)
+                insertAfter = _marbles.Find(_marbles.First());
+
+            var newNode = new LinkedListNode<int>(marble);
+            _marbles.AddAfter(insertAfter, newNode);
+
+            _current = newNode;
+        }
+
+        public override string ToString()
+        {
+            var sArray = new int[_marbles.Count];
+            _marbles.CopyTo(sArray, 0);
+
+            var display = "";
+            for (int i = 0; i < _marbles.Count; i++)
+            {
+                var value = sArray[i];
+                if (value == _current.Value)
+                    display += $"({value}) ";
+                else
+                    display += value + " ";
+            }
+            return display;
+        }
+
+        internal int RemoveMarbles(int marble)
+        {
+            var score = marble;
+
+            var toRemove = _current;
+            for (int i = 0; i < 7; i++)
+            {
+                toRemove = toRemove.Previous;
+                if (toRemove == null)
+                {
+                    toRemove = _marbles.Find(_marbles.Last());
+                }
+            }
+
+            var removing = toRemove.Value;
+            score += removing;
+
+            _current = toRemove.Next;
+            if (_current == null)
+                _current = _marbles.Find(_marbles.First());
+
+            _marbles.Remove(toRemove);
+
+            return score;
+        }
+
+    }
+}
--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Day9
@@ -12,13 +11,13 @@ namespace Day9
             var part2 = Solve(476, 71657 * 100);
 
             Console.WriteLine($"Part 1 ==>  {part1}"); //386018
-            Console.WriteLine($"Part 2 ==>  {part2}"); 
+            Console.WriteLine($"Part 2 ==>  {part2}"); //3085518618
             Console.ReadKey();
         }
 
-        private static int Solve(int numberOfPlayers, int lastMarble)
+        private static long Solve(int numberOfPlayers, int lastMarble)
         {
-            var scores = new int[numberOfPlayers];
+            var scores = new long[numberOfPlayers];
             var circle = new Circle(lastMarble);
             var playerNumber = 0;
             for (int marble = 1; marble <= lastMarble; marble++)
@@ -31,7 +30,7 @@ namespace Day9
                 {
                     var score = circle.RemoveMarbles(marble);
                     scores[playerNumber] += score;
-                    //    Console.WriteLine(score);
+                    //  Console.WriteLine(score);
                 }
 
                 //   System.Console.WriteLine($"[{playerNumber}]  {circle}");
@@ -41,64 +40,6 @@ namespace Day9
 
             var highestScore = scores.Max();
             return highestScore;
-        }
-    }
-
-    class Circle
-    {
-        private readonly List<int> _marbles;
-        private int _currentPosition;
-        public Circle(int maxSize)
-        {
-            _marbles = new List<int>() { 0 };
-            _currentPosition = 1;
-        }
-
-        public void AddMarble(int marble)
-        {
-            if (_marbles.Count == 1)
-            {
-                _marbles.Insert(1, marble);
-                _currentPosition = 1;
-            }
-            else
-            {
-                var locationToInsert = (_currentPosition + 1) % _marbles.Count;
-                _currentPosition = locationToInsert + 1;
-                _marbles.Insert(_currentPosition, marble);
-            }
-        }
-
-        public override string ToString()
-        {
-            var display = "";
-            for (int i = 0; i < _marbles.Count; i++)
-            {
-                if (i == _currentPosition)
-                    display += $"({_marbles[i]}) ";
-                else
-                    display += _marbles[i] + " ";
-            }
-            return display;
-        }
-
-        internal int RemoveMarbles(int marble)
-        {
-            var score = marble;
-
-            //  var toRemove = (_currentPosition - 7) % _marbles.Count;
-            var toRemove = nfmod(_currentPosition - 7, _marbles.Count);
-            score += _marbles[toRemove];
-            _marbles.Remove(_marbles[toRemove]);
-
-            _currentPosition = toRemove % _marbles.Count;
-
-            return score;
-        }
-
-        int nfmod(int a, int b)
-        {
-            return a - b * (int)Math.Floor(a / (double)b);
         }
     }
 }
