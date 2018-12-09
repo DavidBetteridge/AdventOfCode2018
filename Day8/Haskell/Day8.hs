e6a3b1895bf627e88ac8466860f6721dfd813e8b
David Betteridge <DavidaBetteridge@gmail.com>
2018-12-09 00:55:38 +0000

    Works with sample data

--- Day8/Haskell/Day8.hs
+++ Day8/Haskell/Day8.hs
@@ -29,22 +29,23 @@ skipText :: [Char] -> Int -> [Char]
 skipText text 0 = text
 skipText (x:xs) offset = skipText xs (offset - 1)
 
-buildChildNodes :: Int -> [Char] -> Int -> [Node] -> (Int, [Node])
-buildChildNodes 0 text currentOffset builtNodes = (currentOffset, builtNodes)
-buildChildNodes numberOfChild text currentOffset builtNodes = (newOffset, builtNodes ++ [newNode])
+buildChildNodes :: Int -> [Char] -> Int -> (Int, [Node])
+buildChildNodes 0 text currentOffset = (currentOffset, [])
+buildChildNodes numberOfChildren text currentOffset = (finalOffset, newNode : otherNodes)
             where (newOffset, newNode) = buildNode text currentOffset
-                
+                  (finalOffset, otherNodes) = buildChildNodes (numberOfChildren - 1) text newOffset
 
-readMetaData :: Int -> [Char] -> Int -> [Int] -> (Int, [Int])
-readMetaData 0 text currentOffset builtMetaData = (currentOffset, builtMetaData)
-readMetaData numberOfChild text currentOffset builtMetaData = (newOffset, builtMetaData ++ [newMetaData])
+readMetaData :: Int -> [Char] -> Int -> (Int, [Int])
+readMetaData 0 text currentOffset = (currentOffset, [])
+readMetaData numberOfChildren text currentOffset = (finalOffset, newMetaData : otherMetaData)
             where newMetaDataAsText = readInt $ skipText text currentOffset
                   newOffset = currentOffset + 1 + length newMetaDataAsText
                   newMetaData = read newMetaDataAsText :: Int
+                  (finalOffset, otherMetaData) = readMetaData (numberOfChildren - 1) text newOffset
 
 buildNode :: [Char] -> Int -> (Int, Node)
 buildNode contents offset = 
-                        (newOffset , 
+                        (newOffset, 
                         Node { numberOfChildNodes = read theNumberOfChildNodes :: Int, 
                                amountOfMetaData = read theAmountOfMetaData :: Int, 
                                childNodes = theChildNodes, 
@@ -55,8 +56,15 @@ buildNode contents offset =
                         startOfMetaData = offset + 1 + length theNumberOfChildNodes
                         theAmountOfMetaData = readInt $ skipText contents startOfMetaData
                         startOfChildNodes = startOfMetaData + 1 + length theAmountOfMetaData
-                        (startOfMetaDataEntries, theChildNodes) = buildChildNodes (read theNumberOfChildNodes :: Int) contents startOfChildNodes []
-                        (newOffset, theMetaData) = readMetaData (read theAmountOfMetaData :: Int) contents startOfMetaDataEntries []
+                        (startOfMetaDataEntries, theChildNodes) = buildChildNodes (read theNumberOfChildNodes :: Int) contents startOfChildNodes
+                        (newOffset, theMetaData) = readMetaData (read theAmountOfMetaData :: Int) contents (startOfMetaDataEntries)
 
-test = print (newOffset)
+sumMetaDataNodes :: [Node] -> Int                        
+sumMetaDataNodes [] = 0
+sumMetaDataNodes (x:xs) = sumMetaData x + sumMetaDataNodes xs
+
+sumMetaData :: Node -> Int
+sumMetaData node = sum(metaData node) + sumMetaDataNodes (childNodes node)
+
+test = print (sumMetaData n)
     where (newOffset, n) = buildNode "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2" 0
