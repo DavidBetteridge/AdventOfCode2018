c8c6c5f21d2ea56a23c33bd6387aa543e7b97b77
David Betteridge <DavidaBetteridge@gmail.com>
2018-12-11 08:45:54 +0000

    Day 11

---
+++ Day11/Program.cs
@@ -0,0 +1,125 @@
+﻿namespace Day11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.WriteLine($"Part 1 -> {Part1()}");
+
+            Timer.TimeMe(() =>
+            {
+                System.Console.WriteLine($"Part 2 -> {Part2()}");
+            });
+
+            System.Console.ReadKey();
+        }
+
+        private static string Part1()
+        {
+            const int grid_serial_number = 2568;
+            //const int grid_serial_number = 18;
+
+            var powerLevels = new int[300, 300];
+            for (int x = 0; x < 300; x++)
+            {
+                for (int y = 0; y < 300; y++)
+                {
+                    powerLevels[x, y] = CalculatePowerLevel(x, y, grid_serial_number);
+                }
+            }
+
+            var bestTotal = int.MinValue;
+            var topLeftX = 0;
+            var topLeftY = 0;
+            for (int x = 0; x < 300 - 2; x++)
+            {
+                for (int y = 0; y < 300 - 2; y++)
+                {
+                    var regionTotal = powerLevels[x, y] + powerLevels[x + 1, y] + powerLevels[x + 2, y] +
+                                      powerLevels[x, y + 1] + powerLevels[x + 1, y + 1] + powerLevels[x + 2, y + 1] +
+                                      powerLevels[x, y + 2] + powerLevels[x + 1, y + 2] + powerLevels[x + 2, y + 2];
+
+                    if (regionTotal > bestTotal)
+                    {
+                        bestTotal = regionTotal;
+                        topLeftX = x;
+                        topLeftY = y;
+                    }
+                }
+            }
+            return $"{topLeftX},{topLeftY}";
+        }
+        private static string Part2()
+        {
+            const int grid_serial_number = 2568;
+            //const int grid_serial_number = 18;
+
+            var regions = new int[300, 300];
+            var powerLevels = new int[300, 300];
+
+
+            var bestRegionSize = 0;
+            var bestTotal = int.MinValue;
+            var topLeftX = 0;
+            var topLeftY = 0;
+
+            for (int x = 0; x < 300; x++)
+            {
+                for (int y = 0; y < 300; y++)
+                {
+                    powerLevels[x, y] = CalculatePowerLevel(x, y, grid_serial_number);
+                    regions[x, y] = powerLevels[x, y];
+
+                    if (regions[x, y] > bestTotal)
+                    {
+                        bestTotal = regions[x, y];
+                        topLeftX = x;
+                        topLeftY = y;
+                        bestRegionSize = 1;
+                    }
+                }
+            }
+
+            for (int regionSize = 1; regionSize <= 300; regionSize++)
+            {
+                for (int x = 0; x < 300 - regionSize - 1; x++)
+                {
+                    for (int y = 0; y < 300 - regionSize - 1; y++)
+                    {
+                        for (int r = 0; r < regionSize; r++)
+                        {
+                            regions[x, y] += powerLevels[x + (regionSize - 1), y + r];
+                        }
+
+                        for (int c = 0; c < regionSize - 1; c++)
+                        {
+                            regions[x, y] += powerLevels[x + c, y + (regionSize - 1)];
+                        }
+
+                        if (regions[x, y] > bestTotal)
+                        {
+                            bestTotal = regions[x, y];
+                            topLeftX = x;
+                            topLeftY = y;
+                            bestRegionSize = regionSize;
+                        }
+                    }
+                }
+            }
+
+            return $"{topLeftX},{topLeftY},{bestRegionSize}";
+        }
+        private static int CalculatePowerLevel(int x, int y, int grid_serial_number)
+        {
+            var rackID = x + 10;
+            var powerLevel = rackID * y;
+            powerLevel = powerLevel + grid_serial_number;
+            powerLevel = powerLevel * rackID;
+            powerLevel = powerLevel / 100;
+            powerLevel = powerLevel % 10;
+
+            return powerLevel - 5;
+        }
+    }
+}
