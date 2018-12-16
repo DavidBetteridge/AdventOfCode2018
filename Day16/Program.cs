2af61d9178d319c18ffc9b7f2a796aa0a2eae8fb
David Betteridge <david.betteridge@proactis.com>
2018-12-16 21:56:54 +0000

    Day 16 - Part 2

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Day16
@@ -8,13 +10,14 @@ namespace Day16
         static readonly int[] registers = new int[4];
 
         static readonly Action<int, int, int>[] instructions = new Action<int, int, int>[16]
-                { addr, addi, mulr, muli,
-                  banr, bani, borr, bori,
-                  setr, seti, gtir, gtri,
-                  gtrr, eqir, eqri, eqrr };
+                { addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr };
 
         static void Main(string[] args)
         {
+            var opCodesCouldBe = new Dictionary<int, List<string>>();
+            for (int opCode = 0; opCode < 16; opCode++)
+                opCodesCouldBe.Add(opCode, new List<string> { "addr", "addi", "mulr", "muli", "banr", "bani", "borr", "bori", "setr", "seti", "gtir", "gtri", "gtrr", "eqir", "eqri", "eqrr" });
+
             var answer = 0;
             var allLines = System.IO.File.ReadAllLines("Input.txt");
             for (int lineNumber = 0; lineNumber < allLines.Length; lineNumber = lineNumber + 4)
@@ -35,17 +38,65 @@ namespace Day16
 
                 var C = parser.ReadNextInt();
 
-                var after = allLines[lineNumber+2].Replace("After:  [", "").Replace("]", "").Replace(" ", "").Split(',').Select(a => int.Parse(a)).ToArray();
+                var after = allLines[lineNumber + 2].Replace("After:  [", "").Replace("]", "").Replace(" ", "").Split(',').Select(a => int.Parse(a)).ToArray();
 
-                if (FindPossibleInstructions(before, opcode, A, B, C, after) >= 3)
+                if (FindPossibleInstructions(before, opcode, A, B, C, after, opCodesCouldBe) >= 3)
                     answer++;
             }
+            Console.WriteLine("Part 1 is " + answer);
 
-            Console.WriteLine(answer);
+            var opCodeMap = new Action<int, int, int>[16];
+            while (opCodesCouldBe.Any())
+            {
+                foreach (var instruction in instructions)
+                {
+                    var numberOfMatches = 0;
+                    var opCode = -1;
+                    foreach (var item in opCodesCouldBe)
+                    {
+                        if (item.Value.Contains(instruction.Method.Name))
+                        {
+                            numberOfMatches++;
+                            opCode = item.Key;
+                        }
+                    }
+                    if (numberOfMatches == 1)
+                    {
+                        //Console.WriteLine($"{instruction.Method.Name} is {opCode}");
+                        opCodesCouldBe.Remove(opCode);
+                        opCodeMap[opCode] = instruction;
+                    }
+                }
+            }
+
+
+            for (int r = 0; r <= 3; r++)
+                registers[r] = 0;
+
+            using (var fileStream = File.OpenRead("Input Part 2.txt"))
+            using (var streamReader = new StreamReader(fileStream))
+            {
+                String line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    var parser = new Parser(line);
+                    var opcode = parser.ReadNextInt();
+                    parser.Match(' ');
+                    var A = parser.ReadNextInt();
+                    parser.Match(' ');
+                    var B = parser.ReadNextInt();
+                    parser.Match(' ');
+                    var C = parser.ReadNextInt();
+
+                    opCodeMap[opcode](A, B, C);
+                }
+            }
+
+            Console.WriteLine("Part 2 is " + registers[0]);
             Console.ReadKey();
         }
 
-        static int FindPossibleInstructions(int[] registersBefore, int opCode, int A, int B, int C, int[] registersAfter)
+        static int FindPossibleInstructions(int[] registersBefore, int opCode, int A, int B, int C, int[] registersAfter, Dictionary<int, List<string>> opCodesCouldBe)
         {
             var matches = 0;
             foreach (var instruction in instructions)
@@ -65,7 +116,11 @@ namespace Day16
                 if (ok)
                 {
                     matches++;
-                    //Console.WriteLine(instruction.Method.Name);
+                }
+                else
+                {
+                    if (opCodesCouldBe[opCode].Contains(instruction.Method.Name))
+                        opCodesCouldBe[opCode].Remove(instruction.Method.Name);
                 }
             }
             return matches;
