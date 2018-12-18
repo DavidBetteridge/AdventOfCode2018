32be02a0717c08c79b6a63db22cd90ccfeb12ca1
David Betteridge <david.betteridge@proactis.com>
2018-12-18 23:16:52 +0000

    That will do

--- Day16/Day16_FSharp/Program.fs
+++ Day16/Day16_FSharp/Program.fs
@@ -1,96 +1,117 @@
 ﻿open System
 open System.IO
 
-type machineType = { Registers: int[]; }
+type registerState = int[]
+
+type machineType = { Registers: registerState; }
+
+type programLine = { opCode: int;  operandA:int;  operandB:int;  operandC: int}
+
+
+let updateRegisters machine registerNumber newValue =
+    let newRegisters = 
+        match registerNumber with
+        | 0 -> [|newValue;machine.Registers.[1];machine.Registers.[2];machine.Registers.[3];|]
+        | 1 -> [|machine.Registers.[0];newValue;machine.Registers.[2];machine.Registers.[3];|]
+        | 2 -> [|machine.Registers.[0];machine.Registers.[1];newValue;machine.Registers.[3];|]
+        | 3 -> [|machine.Registers.[0];machine.Registers.[1];machine.Registers.[2];newValue;|]
+        | _ -> failwith "Unknown register number"
+    {Registers = newRegisters}
 
 let addr (machine:machineType) A B C = 
-    machine.Registers.[C] <- machine.Registers.[A] + machine.Registers.[B]
+    updateRegisters machine C (machine.Registers.[A] + machine.Registers.[B])
 
 let addi (machine:machineType) A B C = 
-    machine.Registers.[C] <- machine.Registers.[A] + B
+    updateRegisters machine C (machine.Registers.[A] + B)
 
 let mulr (machine:machineType) A B C = 
-    machine.Registers.[C] <- machine.Registers.[A] * machine.Registers.[B]
+    updateRegisters machine C (machine.Registers.[A] * machine.Registers.[B])
 
 let muli (machine:machineType) A B C = 
-    machine.Registers.[C] <- machine.Registers.[A] * B
+    updateRegisters machine C (machine.Registers.[A] * B)
 
 let banr (machine:machineType) A B C = 
-    machine.Registers.[C] <- machine.Registers.[A] &&& machine.Registers.[B]
+    updateRegisters machine C (machine.Registers.[A] &&& machine.Registers.[B])
 
 let bani (machine:machineType) A B C = 
-    machine.Registers.[C] <- machine.Registers.[A] &&& B
+    updateRegisters machine C (machine.Registers.[A] &&& B)
 
 let borr (machine:machineType) A B C = 
-    machine.Registers.[C] <- machine.Registers.[A] ||| machine.Registers.[B]
+    updateRegisters machine C (machine.Registers.[A] ||| machine.Registers.[B])
 
 let bori (machine:machineType) A B C = 
-    machine.Registers.[C] <- machine.Registers.[A] ||| B
+    updateRegisters machine C (machine.Registers.[A] ||| B)
 
 let setr (machine:machineType) A _ C = 
-    machine.Registers.[C] <- machine.Registers.[A]
+    updateRegisters machine C (machine.Registers.[A])
 
 let seti (machine:machineType) A _ C = 
-    machine.Registers.[C] <- A
+    updateRegisters machine C A
 
 let gtir (machine:machineType) A B C = 
-    machine.Registers.[C] <- if ( A > machine.Registers.[B]) then
-                                1
-                             else 
-                                0
-
+    updateRegisters machine C ( if ( A > machine.Registers.[B]) then
+                                    1
+                                else 
+                                    0 )
+    
 let gtri (machine:machineType) A B C = 
-    machine.Registers.[C] <- if ( machine.Registers.[A] > B) then
-                                1
-                             else 
-                                0
+    updateRegisters machine C ( if ( machine.Registers.[A] > B) then
+                                    1
+                                 else 
+                                    0)
 
 let gtrr (machine:machineType) A B C = 
-    machine.Registers.[C] <- if ( machine.Registers.[A] > machine.Registers.[B]) then
-                                1
-                             else 
-                                0
+    updateRegisters machine C ( if ( machine.Registers.[A] > machine.Registers.[B]) then
+                                    1
+                                 else 
+                                    0)
 
 let eqir (machine:machineType) A B C = 
-    machine.Registers.[C] <- if ( A = machine.Registers.[B]) then
-                                1
-                             else 
-                                0
+    updateRegisters machine C ( if ( A = machine.Registers.[B]) then
+                                    1
+                                 else 
+                                    0)
 
 let eqri (machine:machineType) A B C = 
-    machine.Registers.[C] <- if ( machine.Registers.[A] = B) then
-                                1
-                             else 
-                                0
+    updateRegisters machine C ( if ( machine.Registers.[A] = B) then
+                                    1
+                                 else 
+                                    0)
 
 let eqrr (machine:machineType) A B C = 
-    machine.Registers.[C] <- if ( machine.Registers.[A] = machine.Registers.[B]) then
-                                1
-                             else 
-                                0
+    updateRegisters machine C ( if ( machine.Registers.[A] = machine.Registers.[B]) then
+                                    1
+                                 else 
+                                    0 )
 
 let instructions = [("addr",addr); ("addi",addi); ("mulr",mulr); ("muli",muli); ("banr",banr); ("bani",bani); ("borr",borr); ("bori",bori); 
                     ("setr",setr); ("seti",seti); ("gtir",gtir); ("gtri",gtri); ("gtrr",gtrr); ("eqir",eqir); ("eqri",eqri); ("eqrr",eqrr)]
 
 let checkInstruction (registersBefore:int[]) (registersAfter:int[]) A B C (_,fn) =
     let machine = { Registers = Array.init 4 (fun i -> registersBefore.[i]) }
-    fn machine A B C
-    machine.Registers = registersAfter
+    let updatedMachine = fn machine A B C
+    updatedMachine.Registers = registersAfter
 
 let findPossibleInstructions registersBefore registersAfter A B C instructions =
     let couldBeInstruction = checkInstruction registersBefore registersAfter A B C
     instructions |> Seq.where couldBeInstruction
 
+let parseInstruction (line:string) : programLine =
+    let parts = line.Split(' ') |> Array.map int
+    match parts with
+    | [|opCode;A;B;C|] -> { opCode = opCode; operandA=A; operandB=B; operandC=C }
+    | _ -> failwith "Instruction in the wrong format" 
+
 let parseTest (lines:string[]) =
-    let before = lines.[0].Replace(" ","").Split([|"[";",";"]"|], StringSplitOptions.RemoveEmptyEntries) |> Array.skip 1 |> Array.map int
-    let arguments = lines.[1].Split(' ') |> Array.map int
-    let after = lines.[2].Replace(" ","").Split([|"[";",";"]"|], StringSplitOptions.RemoveEmptyEntries) |> Array.skip 1 |> Array.map int
+    let before:registerState = lines.[0].Replace(" ","").Split([|"[";",";"]"|], StringSplitOptions.RemoveEmptyEntries) |> Array.skip 1 |> Array.map int
+    let arguments = parseInstruction lines.[1] 
+    let after:registerState = lines.[2].Replace(" ","").Split([|"[";",";"]"|], StringSplitOptions.RemoveEmptyEntries) |> Array.skip 1 |> Array.map int
     (before, arguments, after)
 
-let runTest (registersBefore, (arguments:int[]), registersAfter) = 
-    let A = arguments.[1]
-    let B = arguments.[2]
-    let C = arguments.[3]
+let runTest (registersBefore, programLine, registersAfter) = 
+    let A = programLine.operandA
+    let B = programLine.operandB
+    let C = programLine.operandC
     findPossibleInstructions registersBefore registersAfter A B C instructions
 
 let rec intersection (listOfinstructions:seq<seq<string>>) : seq<string> =
@@ -118,15 +139,22 @@ let rec workoutOpCode (opCodesAndPossibleInstructions:list<int * seq<string>>) :
             let reducedCodes' = removeInstruction reducedCodes instruction
             (opCode, instruction) :: workoutOpCode reducedCodes'
 
-let parseInstruction (line:string) =
-    line.Split(' ') |> Array.map int
-
-let runProgram (opCodes:list<int * string>) machine instruction =
-    let [|opCode;A;B;C|] = instruction
-    let (_, instructionName) = opCodes |> List.find ( fun (op, _) -> op = opCode)
+let runProgram opCodes machine instruction =
+    let (_, instructionName) = opCodes |> List.find ( fun (op, _) -> op = instruction.opCode)
     let (_, fn) = instructions |> List.find (fun (name, fn) -> name = instructionName)
-    fn machine A B C
-    machine
+    fn machine instruction.operandA instruction.operandB instruction.operandC
+
+let workOutOpCodes (tests:seq<(registerState*programLine*registerState)>) testResults = 
+    let opCodes = tests |> Seq.map (fun ((_, line, _)) -> line.opCode)
+    let names = testResults |> Seq.map (fun results -> results |> Seq.map (fun (name,_) -> name))
+    let multipleOpCodesAndPossibleInstructions = names |> Seq.zip opCodes
+
+    let opCodesAndPossibleInstructions = multipleOpCodesAndPossibleInstructions 
+                                            |> Seq.groupBy (fun (op, _) -> op)
+                                            |> Seq.map (fun (opCode, instructions) -> (opCode, instructions |> Seq.map snd ))
+                                            |> Seq.map (fun (opCode, instructions) -> (opCode, intersection instructions))
+
+    workoutOpCode (List.ofSeq opCodesAndPossibleInstructions)
 
 [<EntryPoint>]
 let main argv =
@@ -142,21 +170,12 @@ let main argv =
 
     printfn "Part 1 is %d" (part1) 
 
-
-    let opCodes = tests |> Seq.map (fun ((_, arguments, _)) -> arguments.[0])
-    let names = testResults |> Seq.map (fun results -> results |> Seq.map (fun (name,_) -> name))
-    let multipleOpCodesAndPossibleInstructions = names |> Seq.zip opCodes
-
-    let opCodesAndPossibleInstructions = multipleOpCodesAndPossibleInstructions 
-                                            |> Seq.groupBy (fun (op, _) -> op)
-                                            |> Seq.map (fun (opCode, instructions) -> (opCode, instructions |> Seq.map snd ))
-                                            |> Seq.map (fun (opCode, instructions) -> (opCode, intersection instructions))
-
-    let opCodes = workoutOpCode (List.ofSeq opCodesAndPossibleInstructions)
+    let opCodes = workOutOpCodes tests testResults
 
     let program = File.ReadLines("Input Part 2.txt") |> Seq.map parseInstruction
     let machine = { Registers = Array.create 4 0}
     let runner = runProgram opCodes
     let finalMachine = program |> Seq.fold runner machine
+
     printfn "Part 2 is %d" finalMachine.Registers.[0]
-    0 // return an integer exit code
+    0 
