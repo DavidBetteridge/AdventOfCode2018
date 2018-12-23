1af54bc704d3d745ab5d73844d81c8137f2d4494
David Betteridge <DavidaBetteridge@gmail.com>
2018-12-23 11:18:03 +0000

    Part 1

---
+++ Day23/Nanobot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day23
+{
+    class Nanobot
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int Z { get; set; }
+        public int SignalRadius { get; set; }
+
+        public Nanobot(string line)
+        {
+            var parser = new Parser(line);
+            parser.Match("pos=<");
+            X = parser.ReadNextInt();
+            parser.Match(",");
+            Y = parser.ReadNextInt();
+            parser.Match(",");
+            Z = parser.ReadNextInt();
+            parser.Match(">, r=");
+            SignalRadius = parser.ReadNextInt();
+        }
+
+        public static int operator -(Nanobot a, Nanobot b) => Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y) + Math.Abs(a.Z - b.Z);
+    }
+}
---
+++ Day23/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nanobots = File.ReadAllLines("Input.txt").Select(line => new Nanobot(line));
+            var strongest = nanobots.MaxBy(nb => nb.SignalRadius);
+            var inRange = nanobots.Count(n => strongest.SignalRadius >= (n - strongest));
+
+            Console.WriteLine("Part 1 " + inRange);
+        }
+
+
+    }
+
+    static class Extensions
+    {
+
+        public static T MaxBy<T>(this IEnumerable<T> source, Func<T, int> action)
+        {
+            var bestMatch = source.FirstOrDefault();
+            var best = action(bestMatch);
+
+            foreach (var item in source)
+            {
+                var score = action(item);
+                if (score > best)
+                {
+                    best = score;
+                    bestMatch = item;
+                }
+            }
+            return bestMatch;
+        }
+    }
+}
